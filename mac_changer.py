deb8637eb9fdd9f5e94dc301395c883d649b0870
DianaNeumann <56086653+DianaNeumann@users.noreply.github.com>
2019-11-13 21:23:09 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -16,15 +16,14 @@ def get_args():
         parser.error("[-] Please specify a mac,use --help for more info")
 
     return values
-  
-  
+
 def change_mac(interface, new_mac):
     print("[+] Changing the mac address of " + interface + " to " + new_mac)
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    
+
     
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig",interface])
@@ -34,7 +33,13 @@ def get_current_mac(interface):
     else:
         print("[-] Could not read MAC address")
 
+        
 values = get_args()
+current_mac = get_current_mac(values.interface)
+print("Current MAC address " + str(current_mac))
 change_mac(values.interface, values.new_mac)
 current_mac = get_current_mac(values.interface)
-print(current_mac)
+if current_mac == values.new_mac:
+    print("[+] MAC was successfully changes to " + current_mac)
+else:
+    print("[-] MAC did not get changes")
